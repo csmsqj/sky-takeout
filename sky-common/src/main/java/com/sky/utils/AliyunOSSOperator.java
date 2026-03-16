06daa1f377bbd23193532502ca6700687c5a0838
csmsqj <3375160014@qq.com>
2026-03-16 21:09:16 +0800

    sky-takeout测试代码

---
+++ sky-common/src/main/java/com/sky/utils/AliyunOSSOperator.java
@@ -0,0 +1,63 @@
+package  com.sky.utils;
+
+import com.aliyun.oss.ClientBuilderConfiguration;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.aliyun.oss.common.comm.SignVersion;
+import com.sky.properties.AliOssProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class AliyunOSSOperator {
+
+
+    private AliOssProperties aliyunOSSProperties;
+    public AliyunOSSOperator(AliOssProperties aliyunOSSProperties){
+        this.aliyunOSSProperties=aliyunOSSProperties;
+    }
+
+//这里可以有参构造注入，或者直接@Autowired注入AliOssProperties对象，这样就可以在这个类中使用AliOssProperties对象了
+    public String upload(byte[] content, String originalFilename) throws Exception {
+        String endpoint = aliyunOSSProperties.getEndpoint();
+String region = aliyunOSSProperties.getRigionId();
+        String bucketName = aliyunOSSProperties.getBucketName();
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+
+        // 填写Object完整路径，例如202406/1.png。Object完整路径中不能包含Bucket名称。
+        //获取当前系统日期的字符串,格式为 yyyy/MM
+        String dir = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM"));
+        //生成一个新的不重复的文件名
+        String newFileName = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
+        String objectName = dir + "/" + newFileName;
+
+        // 创建OSSClient实例。
+        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
+        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
+        OSS ossClient = OSSClientBuilder.create()
+                .endpoint(endpoint)
+                .credentialsProvider(credentialsProvider)
+                .clientConfiguration(clientBuilderConfiguration)
+                .region(region)
+                .build();
+
+        try {
+            ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content));
+        } finally {
+            ossClient.shutdown();
+        }
+
+        return endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + objectName;
+    }
+
+}
