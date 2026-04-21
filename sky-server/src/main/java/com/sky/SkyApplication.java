6b8f9c028da6a87b2d80dc42d5f5f39fd3d47304
csmsqj <3375160014@qq.com>
2026-04-21 12:40:57 +0800

    README

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理,如果不加这个注解，@Transactional注解就不起作用了
-@EnableCaching // 开启注解缓存开关
+@EnableCaching // 开启注解缓存开关, 之后就可以在需要缓存的方法上使用 @Cacheable 注解了
 @EnableScheduling// 开启定时任务
 @Slf4j
 public class SkyApplication {
